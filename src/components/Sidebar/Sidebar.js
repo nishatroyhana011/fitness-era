8b3d6ed748329b75589abffc9f0106f6324c00a5
nishat <nishatroyhana011@gmail.com>
2022-09-29 13:20:11 +0600

    break time updated

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,24 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Myinfo from '../myinfo/Myinfo';
 
 const Sidebar = (props) => {
 
-    const duration = props.duration;
+    const duration = props.duration;    
+
+    const [breakTime, setBreakTime] = useState(0);
+    const getValue = (value) =>{
+        setBreakTime(value);
+    }
+
     return (
         <div className=''>
             <Myinfo></Myinfo>
             <p className='text-teal-500 font-semibold text-xl my-5'>Add a break (minutes)</p>
             <div className='flex justify-around'>
-                <button className="btn text-lg font-bold btn-accent btn-circle">1</button>
-                <button className="btn text-lg font-bold btn-accent btn-circle">3</button>
-                <button className="btn text-lg font-bold btn-accent btn-circle">5</button>
-                <button className="btn text-lg font-bold btn-accent btn-circle">10</button>
+                <button className="btn text-lg font-bold btn-accent btn-circle" onClick={()=>{ getValue(1)}}>1</button>
+                <button className="btn text-lg font-bold btn-accent btn-circle" onClick={()=>{ getValue(3)}}>3</button>
+                <button className="btn text-lg font-bold btn-accent btn-circle" onClick={()=>{ getValue(5)}}>5</button>
+                <button className="btn text-lg font-bold btn-accent btn-circle" onClick={()=>{ getValue(10)}}>10</button>
             </div>
             <div>
             <p className='text-teal-500 font-semibold text-xl my-5'>Exercise Details</p>
             <p className='bg-slate-100 p-5 rounded-lg'>Total Duration: {duration} Minutes</p>
             <br />
-            <p className='bg-slate-100 p-5 rounded-lg'>Break Time: {duration} Minutes</p>
+            <p className='bg-slate-100 p-5 rounded-lg'>Break Time: {breakTime} Minutes</p>
             <br />
             <button className="btn btn-accent btn-block">Activity Completed</button>
             </div>
