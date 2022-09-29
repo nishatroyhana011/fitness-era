9c7d7d6eae4e64a62831ccb80641677f839e3331
nishat <nishatroyhana011@gmail.com>
2022-09-29 20:31:28 +0600

    blog edited

--- src/components/Questions/questions.js
+++ src/components/Questions/questions.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 const Questions = () => {
     return (
-        <div className='my-10'>
+        <div className='my-12'>
+            <p className='text-teal-600 font-bold text-2xl my-5'>Blog Section</p>
             <div>
             <p className='text-teal-600 font-semibold text-xl my-5 mx-10'> How does React work?</p>
             <p>React uses virtual DOM. It uses JSX syntax to create elements.  React divides the UI into seperated reusable pieces of code known as components. 
@@ -13,11 +14,12 @@ const Questions = () => {
             <p className='text-teal-600 font-semibold text-xl my-5 mx-10'> What are the differences between props and state ?</p>
             <p> Props are parameters. It is used to send data from one component to another. 
                 State is used to hold changable data. state has a function and a variable. it works within the same component.
+                Props are read only. state changes may be ascynchoronus.
             </p>
             </div>
             <div>
             <p className='text-teal-600 font-semibold text-xl my-5 mx-10'> What are the functions of useEffect other than data fetching?</p>
-            <p>useEffect is a hook to handle external side effects in react. Without data fetching, it handles direct DOM manipulation, settimeout etc.</p>
+            <p>UseEffect is a hook to handle external side effects in react. React will remember the passed function, and call it later after performing the DOM updates. Without data fetching, it handles direct DOM manipulation, settimeout, localStorage set and update etc.</p>
             </div>
            
         </div>
